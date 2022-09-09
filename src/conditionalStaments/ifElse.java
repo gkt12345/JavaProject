21fe95943242160796cfd790d09cb6be97c1ec7f
Gaurav trivedi <trivedig319@gmail.com>
2022-09-09 17:29:36 +0530

    "inital commit"

---
+++ src/conditionalStaments/ifElse.java
@@ -0,0 +1,12 @@
+package conditionalStaments;
+
+public class ifElse {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+
+       // ternary operator (if(condition) ? case1 : case2)
+
+        System.out.println((x > y) ? x:y);
+    }
+}
