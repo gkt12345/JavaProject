0d85ce478fdec2cee7dbe086a3369ff84ed5b245
Gaurav trivedi <trivedig319@gmail.com>
2022-09-09 17:14:16 +0530

    "inital commit"

---
+++ src/conditionalStaments/main.java
@@ -0,0 +1,15 @@
+package conditionalStaments;
+
+public class main {
+    public static void main (String[] args) {
+        int x = 10;
+        int y = 20;
+
+        if(x > y){
+            System.out.println(x);
+        }
+        else{
+            System.out.println(y);
+        }
+    }
+}
